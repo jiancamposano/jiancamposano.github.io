44882befd091970560d1346e6ce630ded501a160
Jian <jiancamposano17@gmail.com>
2022-11-25 15:55:59 -0500

    Misionvcision

---
+++ Mocayas/js/enviar.js
@@ -0,0 +1,34 @@
+/* function hizoClick() {
+    const nombre = document.getElementById('nombre');
+    if (nombre.value === "") {
+        alert("Por favor, escribe tu nombre de usuario.");
+        nombre.focus();
+       
+      }else{
+          alert("Mensaje Enviado");
+
+      }
+  } */
+
+
+  /* const btnEnviar = document.getElementById('btn-enviar');
+
+const validacion = (e) => {
+  e.preventDefault();
+  const nombre = document.getElementById('nombre');
+  const email = document.getElementById('email');
+  if (nombre.value === "") {
+    alert("Por favor, escribe tu nombre de usuario.");
+    nombre.focus();
+    return false;
+  }
+  if (email.value === "") {
+    alert("Por favor, escribe tu correo electrónico");
+    email.focus();
+    return false;
+  }
+  
+  return true;
+}
+
+btnEnviar.addEventListener('click', validacion()); */
